575b2f19c96f5c252f306a15fe1156cb35a34ddb
Nicolas Rissi <nicolasrissi15@gmail.com>
2024-12-04 22:55:53 -0300

    Primeiro commit

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MauiAppHotel.Models;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public List<Quarto> Lista_quartos 
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
